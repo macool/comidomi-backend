6bc1e65ccbaa3aa4dea1d3ff0950013099f363c7
Mario Andrés Correa <a@macool.me>
2019-02-13 13:56:40 -0500

    update airbrake key

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '4d25cc18c803023c3c9ba68f6dc26a2f'
+  config.api_key = '44efb53b680822cba2a41aa454f5ab1c'
   config.host    = 'pangi.shiriculapo.com'
   config.port    = 80
   config.secure  = config.port == 443
