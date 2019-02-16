6f7215d734cb495cdd6bad2f22942daeefe54138
Mario Andrés Correa <a@macool.me>
2019-02-16 10:56:25 -0500

    updating pusher credentials

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,5 +3,6 @@ require 'pusher'
 Pusher.key = Rails.application.secrets.pusher_key
 Pusher.secret = Rails.application.secrets.pusher_secret
 Pusher.app_id = Rails.application.secrets.pusher_app_id
+Pusher.cluster = Rails.application.secrets.pusher_cluster
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
